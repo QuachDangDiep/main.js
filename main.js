edaa015a43b0aa827e526aa5f68fbb952d020c04
QuachDangDiep <145833428+QuachDangDiep@users.noreply.github.com>
2024-01-08 22:36:12 +0700

    up

--- main.js
+++ main.js
@@ -62,14 +62,124 @@ Các loại function
 
 
 //Polyfill ? (30)
-if (!String.prototype.includes) {
-    String.prototype.includes = function(search, start) {
-        'use strict';
+// if (!String.prototype.includes) {
+//     String.prototype.includes = function(search, start) {
+//         'use strict';
 
-        if (search instanceof RegExp) {
-            throw TypeError('first argument must not be a RegExp');
-        }
-        if (start === undefined) { start =0; }
-        return this.indexOf(search,start) !== -1;
-    };
-}
+//         if (search instanceof RegExp) {
+//             throw TypeError('first argument must not be a RegExp');
+//         }
+//         if (start === undefined) { start =0; }
+//         return this.indexOf(search,start) !== -1;
+//     };
+// }
+
+// For loop
+// var myArray = [
+//     'Javascript',
+//     'PHP',
+//     'Java',
+//     'Dart',
+//     'Ruby',
+//     'Python'
+// ];
+
+// // console.log(myArray[0]);
+// var arrayLength = myArray.length;
+
+// for (var i = 0; i < arrayLength; i++) {
+//     console.log(myArray[i]);
+// }
+
+// For/in loop  1
+ 
+// var myInfo = {
+//     name: 'Dang Diep',
+//     age: 18,
+//     address: 'Bac Ninh, VN'
+// };
+
+// for (var key in myInfo) {
+//     console.log(myInfo[key]);
+// }
+
+// var languages = [ 2
+//     'Javascript',
+//     'PHP',
+//     'Ruby',
+//     'Java'
+// ];
+
+// for (var key in languages) {
+//     console.log(languages[key]);
+// }
+
+// var languages = 'Javascript';  3
+
+// for (var key in languages) {
+//     console.log(languages[key]);
+// }
+
+// For/of loop
+
+// var languages = [ 1 mang
+//     'Javascript',
+//     'PHP',
+//     'Java'
+// ];
+
+// for (var value of languages) {
+//     console.log(value);
+// }
+
+// var languages = 'Javascript'; 2 chuoi
+
+// for (var value of languages) {
+//     console.log(value);
+// }
+
+// var myInfo = {
+//     name: 'Dang Diep',
+//     age: '18',
+//     address: 'Bac Ninh, VN'
+// };
+
+// // console.log(Object.values(myInfo));
+// // console.log(Object.keys(myInfo))
+
+// for (var value of Object.values(myInfo)) {
+//     console.log(value);
+// }
+
+
+// while loop
+
+// var myArray = [
+//     'Javascript',
+//     'PHP',
+//     'Ruby'
+// ];
+
+// var i = 0;
+
+// while (i < myArray.length) {
+//     console.log(myArray[i]);
+//     i++;
+// }
+
+// do/while loop
+
+var i = 0;
+var isSuccess = false;
+
+do{
+   i++;
+
+   console.log('Nạp thẻ lần' + i);
+
+   // Thành công
+   if(false) {
+      isSuccess = true;
+   }
+
+} while (!isSuccess && i <= 3);
